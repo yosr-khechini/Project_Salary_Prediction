b821b50647b82085f459a2c4e3e31a4450dcaa4f
Yosr Khechini <khechiniyosr@gmail.com>
2026-01-23 16:29:18 +0100

    "Version 10: reorganized files, deleted some "

--- models.py
+++
@@ -1,8 +0,0 @@
-from flask_login import UserMixin
-from Project_Salary_Prediction import db
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    name = db.Column(db.String(100))
-    password = db.Column(db.String(200))
